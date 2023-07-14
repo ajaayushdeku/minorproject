32a66496431f7364860c2249d80666f560ef384b
ajaayushdeku <be2020se698@gces.edu.np>
2023-07-15 00:27:25 +0545

    dashboard reponsive update

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const navElement = document.querySelector(".nav");
+const sideElement = document.querySelector(".side-list");
 const hamburgerElement = document.querySelector(".hamburger");
+const hamburger1Element = document.querySelector(".hamburger1");
 
 
 hamburgerElement.addEventListener("click",()=> {
-    navElement.classList.toggle(".nav--open");
-})
+    navElement.classList.toggle("nav--open");
+});
+
+hamburger1Element.addEventListener("click",()=> {
+    sideElement.classList.toggle("nav--open");
+});
